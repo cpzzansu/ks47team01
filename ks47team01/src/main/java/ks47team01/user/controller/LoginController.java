9cf000348ebe13c882afc47452082cf09f94e983
EterSide <cjsaus0216@gmail.com>
2023-07-06 10:15:15 +0900

    [ksmart47] author: 박지성 폴더와 html 생성 컨트롤러 수정

--- ks47team01/src/main/java/ks47team01/user/controller/LoginController.java
+++ ks47team01/src/main/java/ks47team01/user/controller/LoginController.java
@@ -3,25 +3,26 @@ package ks47team01.user.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/Ulogin/user_login_action")
+    @PostMapping("/userLogin/userLogin")
     public String userLoginAction(Model model) {
         model.addAttribute("title", "urbanfarm");
         return "user_login/main_login";
     }
 
     // 로그인화면
-    @GetMapping("/Ulogin/user_login")
+    @GetMapping("/userLogin/userLogin")
     public String loginForm(Model model) {
         model.addAttribute("title", "urbanfarm");
         return "user_login/user_login";
     }
     
-    @GetMapping("/Ulogin/add_user")
+    @GetMapping("/userLogin/add_user")
     public String addForm(Model model) {
     	
     	
--- ks47team01/src/main/java/ks47team01/user/controller/UserAddController.java
+++ ks47team01/src/main/java/ks47team01/user/controller/UserAddController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserAddController {
 
-    @GetMapping("Uin/user_add_form")
-    public String userInsertForm(Model model) {
+    @GetMapping("/userAdd/userAddForm")
+    public String userAddForm(Model model) {
         model.addAttribute("title", "urbanfarm_회원가입");
         model.addAttribute("urbanfarmer", "1");
         return "/user_add/user_add_form";
