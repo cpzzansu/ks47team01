a45760434061e183dd201109d9fa18f19371e8c6
EterSide <cjsaus0216@gmail.com>
2023-07-07 12:12:36 +0900

    /를 안쓴거 수정

--- ks47team01/src/main/java/ks47team01/admin/controller/AdminAddrController.java
+++ ks47team01/src/main/java/ks47team01/admin/controller/AdminAddrController.java
@@ -6,28 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminAddrController {
 	
-	@GetMapping("adminAddr/adminAddrList")
+	@GetMapping("/adminAddr/adminAddrList")
 	public String adminAddrList() {
 		
 		return "admin_addr/addr_admin_list";
 		
 	}
 	
-	@GetMapping("adminAddr/adminRemoveAddr")
+	@GetMapping("/adminAddr/adminRemoveAddr")
 	public String adminRemoveAddr() {
 		
 		return "admin_addr/remove_admin_addr";
 		
 	}
 	
-	@GetMapping("adminAddr/adminSearchAddr")
+	@GetMapping("/adminAddr/adminSearchAddr")
 	public String adminSearchAddr() {
 		
 		return "admin_addr/search_admin_addr";
 		
 	}
 
-	@GetMapping("adminAddr/adminUpdateAddr")
+	@GetMapping("/adminAddr/adminUpdateAddr")
 	public String adminUpdateAddr() {
 		
 		return "admin_addr/update_admin_addr";
