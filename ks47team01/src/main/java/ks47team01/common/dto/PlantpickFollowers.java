a645018fe49cd2908d4c86f4f6fa178e5dcadd71
gimsang-won <gimsang-won@218.38.137.28>
2023-07-17 17:24:57 +0900

    [김상원 - 월요일 작업]

---
+++ ks47team01/src/main/java/ks47team01/common/dto/PlantpickFollowers.java
@@ -0,0 +1,16 @@
+package ks47team01.common.dto;
+
+import lombok.Data;
+
+@Data
+public class PlantpickFollowers {
+	
+	//db보고 만
+	private String plantpickFollowersCode;
+	private String urbanfarmerFollowerId;
+	private String urbanfarmerId;
+	private String plantpickFollowersRegDate;
+	private Urbanfarmer urbanfarmer;
+	
+	
+}
