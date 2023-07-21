b3444770d9a2a2811d517f8a6c60a0a823454f2a
donghun111 <dldmldn159@naver.com>
2023-07-21 17:20:29 +0900

    [ks47team01] 이동훈 detail: plan_main에 modal추가

--- ks47team01/src/main/java/ks47team01/user/controller/PlanController.java
+++ ks47team01/src/main/java/ks47team01/user/controller/PlanController.java
@@ -1,5 +1,6 @@
 package ks47team01.user.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpSession;
@@ -98,6 +101,15 @@ public class PlanController {
 		return "redirect:/userPlan/planMain";
 	}
 	
+	@PostMapping("/ajax/getCropsInfo")
+	@ResponseBody
+	public Map getCropsInfo(@RequestBody String farmerFarmingPlanCode) {
+		System.out.println("테스트");
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("id", "12");
+		resultMap.put("pw", "as");
+		return resultMap;
+	}
 	
 	/**
 	 * 키울 작물 삭제화면
