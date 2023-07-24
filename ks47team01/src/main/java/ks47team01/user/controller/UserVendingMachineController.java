1cda87161e6bab537bb104fcbb7bc76ed67728ce
ASUS <ASUS@DESKTOP-T50VO8O>
2023-07-24 17:15:39 +0900

    0724 자판기홈->지역->동네까지 db연결 했습니다.

--- ks47team01/src/main/java/ks47team01/user/controller/UserVendingMachineController.java
+++ ks47team01/src/main/java/ks47team01/user/controller/UserVendingMachineController.java
@@ -1,11 +1,23 @@
 package ks47team01.user.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import ks47team01.common.dto.HubLargeCate;
+import ks47team01.common.dto.HubMidCateFinal;
+import ks47team01.common.dto.VendingMachine;
+import ks47team01.user.service.UserVmService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Controller
 public class UserVendingMachineController {
+	
+	private final UserVmService userVmService;
 
 	// 유저 자판기 메인화면
 	@GetMapping("/userVendingMachine/vmMain")
@@ -17,21 +29,32 @@ public class UserVendingMachineController {
 	// 자판기 사용 지역 선택 화면
 	@GetMapping("/userVendingMachine/vmLocalSelect")
 	public String userVmLocalSelect (Model model) {
+		
+		List<HubLargeCate> getHubList = userVmService.getHubList();
+		
 		model.addAttribute("title", "지역 선택");
+		model.addAttribute("getHubList", getHubList);
+		
 		return "user_vending_machine/vm_local_select";
 	}
 
 	// 자판기 사용 군/구/동 선택 화면
 	@GetMapping("/userVendingMachine/vmAreaSelect")
-	public String userVmAreaSelect (Model model) {
+	public String userVmAreaSelect (Model model,
+									@RequestParam(value = "hubLargeCateCode")String hubLargeCateCode) {
+		List<HubMidCateFinal> getAreaList = userVmService.getAreaList(hubLargeCateCode);
 		model.addAttribute("title", "동네 선택");
+		model.addAttribute("getAreaList", getAreaList);
 		return "user_vending_machine/vm_area_select";
 	}
 	
 	// 선택한 허브 내 자판기 선택 화면
 	@GetMapping("/userVendingMachine/vmMachineSelect")
-	public String userVmMachineSelect (Model model) {
+	public String userVmMachineSelect (Model model,
+									@RequestParam(value = "hubMidCateFinalCode")String hubMidCateFinalCode) {
+		List<VendingMachine> getVmList = userVmService.getVmList(hubMidCateFinalCode); 
 		model.addAttribute("title", "자판기 선택");
+		model.addAttribute("getVmList", getVmList);
 		return "user_vending_machine/vm_machine_select";
 	}
 	
--- ks47team01/src/main/java/ks47team01/user/mapper/UserVmMapper.java
+++ ks47team01/src/main/java/ks47team01/user/mapper/UserVmMapper.java
@@ -1,9 +1,21 @@
 package ks47team01.user.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import ks47team01.common.dto.HubLargeCate;
+import ks47team01.common.dto.HubMidCateFinal;
+import ks47team01.common.dto.VendingMachine;
+import ks47team01.common.dto.HubMidCateFinal;
+import ks47team01.common.dto.HubMidCateFinal;
 
 @Mapper
 public interface UserVmMapper {
 
+	public List<HubLargeCate> getHubList();
 	
+	public List<HubMidCateFinal> getAreaList(String hubMidCateFinal);
+
+	public List<VendingMachine> getVmList(String vendingMachine);
 }
